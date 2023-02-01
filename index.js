1bdea37afe6668766012f9623bbb5a152fa0dd59
alikaiwe <alikaiwe@gmail.com>
2023-02-01 18:07:30 +0100

    created index js file

---
+++ index.js
@@ -0,0 +1,11 @@
+class bottle{
+    bottle(a, b){
+        this.size = a
+        this. length = b
+        this.total = 0
+    }
+
+    area(){
+        return this.total = this.size * this.length
+    }
+}
